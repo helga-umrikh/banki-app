96a65b824878566d4164049a466182b240ec3d0e
helga-umrikh <zetaumr@gmail.com>
2024-06-28 13:44:47 +0300

    fix: remove unnecessary stuff, fix key prop

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import './List.scss'
 import { Avatar, List as ListComponent, Typography } from 'antd'
 import { IProduct } from '../../interfaces/IBankData'
-const { Title } = Typography
+const { Title, Text } = Typography
 interface ListProps {
     data: IProduct[]
 }
@@ -17,10 +17,10 @@ const List: FC<ListProps> = ({ data }) => {
         >
             {data.length > 0 ? (
                 data.map((item) => (
-                    <ListComponent.Item>
+                    <ListComponent.Item key={item.name}>
                         <ListComponent.Item.Meta
                             avatar={<Avatar src={item.logo} shape="square" />}
-                            title={<Title level={5}>{item.name}</Title>}
+                            title={<Text strong>{item.name}</Text>}
                             description="сумма"
                         />
                         <Title level={5}>
